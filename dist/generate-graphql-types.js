5f852b80990b8303e89caab4c5e5985f469c52d7
Ivo Katunaric <ivo@coreline.agency>
2018-11-24 15:50:14 +0100

    Rebuild

--- dist/generate-graphql-types.js
+++ dist/generate-graphql-types.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var child_process_1 = require("child_process");
 var util_1 = require("util");
 var execAsync = util_1.promisify(child_process_1.exec);
-execAsync('npx apollo codegen:generate src/graphql/types.ts').then(function (_a) {
+var command = "npx apollo codegen:generate src/graphql/types.ts --schema schema.json --target typescript --outputFlat --queries 'src/**/**queries.ts'";
+execAsync(command).then(function (_a) {
     var stderr = _a.stderr, stdout = _a.stdout;
     console.log(stdout);
     console.error(stderr);
