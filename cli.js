8061ac06ee6d53754f7c854d8f3418f1a4961862
Ivo Katunaric <ivo@coreline.agency>
2018-11-24 15:15:48 +0100

    Use ts-node

--- cli.js
+++ cli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
+require('ts-node/register');
 
 if (process.argv[2] === 'generate:store') {
-  require('./dist/generate-graphql-store');
+  require('./generate-graphql-store');
 } else {
   throw new Error('generate:store argument expected');
 }
